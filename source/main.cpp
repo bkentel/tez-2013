7b193aa08e78e60d33669ed82c433286dbe39504
Brandon Kentel <>
2013-10-11 12:11:42 +0900

    more work on iterators

--- source/main.cpp
+++ source/main.cpp
@@ -259,6 +259,19 @@ struct index2d {
     T x, y;
 };
 
+template <typename T>
+struct point2d {
+    T x, y;
+};
+
+template <typename T>
+void reorder(point2d<T>& pa, point2d<T>& pb) {
+    using std::swap;    
+
+    if (pb.x < pa.x) swap(pa.x, pb.x);
+    if (pb.y < pa.y) swap(pa.y, pb.y);
+}
+
 template <typename T, typename IndexType>
 struct grid_itererator_value {
     grid_itererator_value operator=(grid_itererator_value const&) = delete;    
@@ -340,13 +353,11 @@ public:
     }
 
     void decrement() {
-        base_reference() = base() - 1;
-        --pos_;
+        advance(-1);
     }
 
     void increment() {
-        base_reference() = base() + 1;
-        ++pos_;
+        advance(1);
     }
    
     size_t    pos_;
@@ -354,16 +365,95 @@ public:
     IndexType height_;
 };
 
+template <typename T, typename IndexType>
+class grid_sub_itererator
+  : public boost::iterator_adaptor<
+        grid_sub_itererator<T, IndexType>                            // Derived
+      , typename grid_itererator_traits<T, IndexType>::base      // Base
+      , typename grid_itererator_traits<T, IndexType>::value     // Value
+      , typename grid_itererator_traits<T, IndexType>::traversal // CategoryOrTraversal
+      , typename grid_itererator_traits<T, IndexType>::reference // Reference
+    >
+{
+public:
+    grid_sub_itererator() BK_NOEXCEPT
+      : grid_sub_itererator::iterator_adaptor_ {}
+      , width_ {0}, height_ {0}, pos_ {}
+    {
+    }
+
+    grid_sub_itererator(base_type it, size_t offset, size_t stride, IndexType w, IndexType h)
+      : grid_sub_itererator::iterator_adaptor_ {it + offset}
+        , width_ {w}, height_ {h}, pos_ {0}, offset_ {offset}, stride_ {stride}
+    {
+    }
+
+    template <typename U>
+    grid_sub_itererator(
+        grid_sub_itererator<U, IndexType> const& other
+      , typename std::enable_if<std::is_convertible<U*,T*>::value>::type* = nullptr
+    )
+        : grid_sub_itererator::iterator_adaptor_(other.base())
+    {
+    }
+ private:
+    friend class boost::iterator_core_access;
+
+    typename iterator_adaptor::reference dereference() const {
+        auto const x = (pos_ % width_) + (stride_ - width_);
+        auto const y = (pos_ + offset_) / stride_;
+
+        return grid_itererator_value<T, IndexType>(
+            *base(), {static_cast<IndexType>(x), static_cast<IndexType>(y) }
+        );
+    }
+
+    void advance(typename iterator_adaptor::difference_type n) {
+        auto dx = (pos_ + n) % width_;
+        auto dy = ((pos_ + n) / width_) - (pos_ / width_);
+
+        BK_ASSERT(pos_ + n >= 0);
+        BK_ASSERT(pos_ + n <= width_ + height_);
+
+        if (pos_ + n == width_ + height_) {
+            base_reference() = base() + n;
+        } else {
+            base_reference() = base() + dx + dy*(stride_ - width_ + 1);    
+        }
+        
+        pos_ += n;
+    }
+
+    void decrement() {
+        advance(-1);
+    }
+
+    void increment() {
+        advance(+1);
+    }
+   
+    size_t pos_;
+    size_t offset_;
+    size_t stride_; 
+
+    IndexType width_;
+    IndexType height_;
+};
+
 template <typename T, typename IndexType = int>
 class grid2d {
 public:
-    using index_t = index2d<IndexType>;
+    using point = point2d<IndexType>;
+    using index_t = point;
 
     using reference = T&;
     using const_reference = T const&;
 
     using iterator       = grid_itererator<T,       IndexType>;
     using const_iterator = grid_itererator<T const, IndexType>;
+
+    using sub_iterator       = grid_sub_itererator<T,       IndexType>;
+    using const_sub_iterator = grid_sub_itererator<T const, IndexType>;
 
     grid2d(IndexType w, IndexType h, T value = T {})
         : width_  { [&] { BK_ASSERT(w > 0); return w; }() }
@@ -385,6 +475,34 @@ public:
             && (i.x < width_) && (i.y < height_);
     }
 
+    sub_iterator begin(point pa, point pb) {
+        reorder(pa, pb);
+        
+        auto const w = pb.x - pa.x;
+        auto const h = pb.y - pa.y;
+
+        BK_ASSERT(w > 0);
+        BK_ASSERT(h > 0);
+
+        return sub_iterator(data_.begin(), index2d_to_index_(pa), width_, w, h);
+    }
+    
+    sub_iterator end(point pa, point pb) {
+        reorder(pa, pb);
+
+        auto const w = pb.x - pa.x;
+        auto const h = pb.y - pa.y;
+
+        BK_ASSERT(w > 0);
+        BK_ASSERT(h > 0);
+
+        //the recct formed by pa and pb is exclusive of pb; make the iterator
+        //point to one element past the last element.
+        --pb.y;
+
+        return sub_iterator(data_.begin(), index2d_to_index_(pb), 0, 0, 0);
+    }
+    
     iterator begin() { return iterator(data_.begin(), width_, height_); }
     iterator end() { return iterator(data_.end(), width_, height_); }
 
@@ -404,29 +522,130 @@ private:
 };
 
 template <typename T>
-index2d<T> north(index2d<T> i) BK_NOEXCEPT {
-    return {i.x, i.y - 1};
+index2d<T> north(index2d<T> i, typename std::make_signed<T>::type n = 1) BK_NOEXCEPT {
+    return {i.x, i.y - n};
 }
     
 template <typename T>
-index2d<T> south(index2d<T> i) BK_NOEXCEPT {
-    return {i.x, i.y + 1};
+index2d<T> south(index2d<T> i, typename std::make_signed<T>::type n = 1) BK_NOEXCEPT {
+    return {i.x, i.y + n};
 }
 
 template <typename T>
-index2d<T> east(index2d<T> i) BK_NOEXCEPT {
-    return {i.x + 1, i.y};
+index2d<T> east(index2d<T> i, typename std::make_signed<T>::type n = 1) BK_NOEXCEPT {
+    return {i.x + n, i.y};
 }
 
 template <typename T>
-index2d<T> west(index2d<T> i) BK_NOEXCEPT {
-    return {i.x - 1, i.y};
+index2d<T> west(index2d<T> i, typename std::make_signed<T>::type n = 1) BK_NOEXCEPT {
+    return {i.x - n, i.y};
 }
 
 template <typename T>
 T const& as_const(T& x) BK_NOEXCEPT {
     return x;
 }
+
+
+
+template <typename T>
+class axis_aligned_rect {
+public:
+    using point = point2d<T>;
+
+    axis_aligned_rect(T x0, T y0, T x1, T y1)
+        : x0_ {x0}, y0_ {y0}
+        , x1_ {x1}, y1_ {y1}
+    {
+        BK_ASSERT(x1 > x0);
+        BK_ASSERT(y1 > y0);
+    }
+
+    axis_aligned_rect(std::initializer_list<T> list)
+        : axis_aligned_rect(
+            *(list.begin() + 0)
+          , *(list.begin() + 1)
+          , *(list.begin() + 2)
+          , *(list.begin() + 3)
+        )
+    {
+        BK_ASSERT(list.size() == 4);        
+    }
+
+    axis_aligned_rect(point p, T w, T h)
+        : axis_aligned_rect(p.x, p.y, p.x + w, p.y + h)
+    {
+    }
+
+    void move_to(point p) {
+        auto const dx = p.x - x0_;
+        auto const dy = p.y - y0_;
+
+        x0_ += dx; x1_ += dx;
+        y0_ += dy; y1_ += dy;
+    }
+
+    T left()   const BK_NOEXCEPT { return x0_; }
+    T right()  const BK_NOEXCEPT { return x1_; }
+    T top()    const BK_NOEXCEPT { return y0_; }
+    T bottom() const BK_NOEXCEPT { return y1_; }
+
+    T width()  const BK_NOEXCEPT { return right()  - left(); }
+    T height() const BK_NOEXCEPT { return bottom() - top();  }
+private:
+    bool is_well_formed() const {
+        return (left() < right()) && (top() < bottom());
+    }
+
+    T x0_, x1_;
+    T y0_, y1_;
+};
+
+template <typename T>
+std::pair<bool, axis_aligned_rect<T>>
+intersect(axis_aligned_rect<T> const ra, axis_aligned_rect<T> const rb) {
+    T const left   = std::max(ra.left(),   rb.left());
+    T const right  = std::min(ra.right(),  rb.right());
+    T const top    = std::max(ra.top(),    rb.top());
+    T const bottom = std::min(ra.bottom(), rb.bottom());
+
+    if (left < right && top < bottom) {
+        return { true, {left, top, right, bottom} };
+    } else {
+        return { false, {0, 0, 1, 1} };
+    }
+}
+
+struct tile {
+    enum class tile_type : uint16_t {
+        empty = 0,
+    };
+
+    tile_type type;
+    uint64_t  data;
+};
+
+class level_map {
+    using rect = axis_aligned_rect<int>;
+
+    rect find_space_for(rect ra) const {
+        ra.move_to(rect::point {{0, 0}});
+
+        for (auto const& rb : room_rects_) {
+            auto i = intersect(ra, rb);
+
+            if (i.first) {
+                
+            } else {
+                return ra;
+            }
+        }
+    }
+
+    grid2d<tile>      grid_;
+    std::vector<rect> room_rects_;
+};
+
 
 void main()
 try {
@@ -438,16 +657,18 @@ try {
         cell.value = out.str();
     }
 
-    for (auto cell : as_const(test_grid)) {
+    //for (auto cell : as_const(test_grid)) {
+    //    std::cout << cell.value << std::endl;
+    //}
+
+    using point = grid2d<std::string>::point;
+    auto beg = test_grid.begin(point {2, 2}, point {4, 4});
+    auto end = test_grid.end(point {2, 2}, point {4, 4});
+
+    for (auto it = beg; it != end; ++it) {
+        auto const& cell = *it;
         std::cout << cell.value << std::endl;
-
-        if ( test_grid.is_valid(north(cell.i)) ) {
-            std::cout << "  north = " << test_grid[north(cell.i)] << std::endl;
-        } else {
-            std::cout << "  north = <NONE>" << std::endl;
-        }
     }
-
 
     bklib::platform_window win {L"Tez"};
 
